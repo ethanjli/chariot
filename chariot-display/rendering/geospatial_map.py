75b12dd0254d9cdd8ca6e4f1776c20a7ce2bdb0c
Ethan Li <lietk12@gmail.com>
2017-08-12 22:23:03 -0700

    Split location indicator into initialization and update methods.

--- chariot-display/rendering/geospatial_map.py
+++ chariot-display/rendering/geospatial_map.py
@@ -1,17 +1,20 @@
 """Classes for rendering geospatial maps."""
 import math
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import cartopy
 
 class MapCanvas(object):
     def __init__(self):
+        self.fig = plt.figure()
         self.ax = plt.axes(projection=cartopy.crs.Mercator())
+        self._location_indicator_size = None
 
     def start_rendering(self):
         plt.show()
 
-    def populate_map(self, dataset):
+    def initialize_map(self, dataset):
         self.ax.add_feature(dataset.get_feature('roads'), edgecolor='black', facecolor='')
         self.ax.add_feature(dataset.get_feature('buildings'),
                             edgecolor='gray', facecolor='gray')
@@ -20,7 +23,7 @@ class MapCanvas(object):
         self.ax.add_feature(dataset.get_feature('natural'), edgecolor='green', facecolor='')
         self.ax.set_extent(dataset.bounds)
 
-    def plot_track(self, track, longitude_margins=0.001, latitude_margins=0.001):
+    def initialize_track(self, track, longitude_margins=0.001, latitude_margins=0.001):
         plt.plot(track.longitudes, track.latitudes, transform=cartopy.crs.PlateCarree(),
                  color='blue')
         bounds = list(track.bounds)
@@ -30,11 +33,20 @@ class MapCanvas(object):
         bounds[3] += latitude_margins
         self.ax.set_extent(bounds)
 
-    def plot_location_indicator(self, x, y, dx, dy, size=0.00025):
+    def initialize_location_indicator(self, track, size=0.0002):
+        self._location_indicator_size = size
+        self.indicator = self._draw_location_indicator(*track.coordinates_and_deltas[0])
+
+    def _draw_location_indicator(self, x, y, dx, dy):
         length = math.sqrt(dx * dx + dy * dy)
-        self.indicator = self.ax.arrow(x, y, dx * size / length, dy * size / length,
-                                       transform=cartopy.crs.PlateCarree(),
-                                       length_includes_head=True, width=0,
-                                       head_length=size, head_width=size, overhang=-0.2,
-                                       linewidth=0)
+        size = self._location_indicator_size
+        return self.ax.arrow(x, y, dx * size / length, dy * size / length,
+                             transform=cartopy.crs.PlateCarree(),
+                             length_includes_head=True, width=0,
+                             head_length=size, head_width=size, overhang=-0.2,
+                             linewidth=0)
+
+    def update_location_indicator(self, x, y, dx, dy):
+        self.indicator.remove()
+        self._draw_location_indicator(x, y, dx, dy)
 
